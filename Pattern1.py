b6974731c8c8871c06d6b8f4a396ef8062583cba
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2021-03-12 21:33:13 +0530

    Pattern1 done

---
+++ Pattern1.py
@@ -0,0 +1,13 @@
+# Pattern 1
+#      *
+#     **
+#    ***
+#   ****
+#  *****
+
+for i in range(5):
+	for j in range(5-i-1):
+		print(" ",end="")
+	for k in range(i+1):
+		print("*",end="")
+	print()
