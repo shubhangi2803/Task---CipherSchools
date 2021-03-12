233cfacf017e389d1cad7c0881e06366009bdd89
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2021-03-12 23:09:08 +0530

    Pattern 1 done

--- Pattern1.py
+++ Pattern1.py
@@ -6,8 +6,13 @@
 #  *****
 
 for i in range(5):
+
+    # For printing spaces
 	for j in range(5-i-1):
 		print(" ",end="")
+
+    # For printing *
 	for k in range(i+1):
 		print("*",end="")
+        
 	print()
