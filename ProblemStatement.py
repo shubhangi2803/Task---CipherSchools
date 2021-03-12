35fe0e878b8129dee3e411e85571ad1733344625
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2021-03-12 22:43:36 +0530

    Problem Statement done

--- ProblemStatement.py
+++ ProblemStatement.py
@@ -8,16 +8,16 @@ while True:
         print("Please enter correct value of budget.")
 
 while True:
-	print("1. Add an item")
-	print("2. Exit")
+    print("1. Add an item")
+    print("2. Exit")
     while True:
         try:
-        	choice = int(input("Enter your choice: "))
+            choice = int(input("Enter your choice: "))
             break
         except:
             print("Please enter correct choice.")
-
-	if choice == 1:
+    
+    if choice == 1:
         while True:
             try:
                 pro = input("Enter product: ")
@@ -26,35 +26,35 @@ while True:
                 break
             except:
                 print("Please enter all values correctly.")
-
-		if amt > budget:
-			print("Can't buy the product as budget left is : ",budget)
+        
+        if amt > budget:
+            print("Can't buy the product as budget left is : ",budget)
             continue
-
-		else:
-			if pro in groceryList:
-				groceryList[pro][0] = qty
-				budget += groceryList[pro][1]
-				groceryList[pro][1] = amt
-			else:
-				groceryList[pro] = [qty, amt]
-			budget -= amt
-
-	elif budget == 0 or choice == 2:
-		break
-	print("Amount left: ",budget)
+        
+        else:
+            if pro in groceryList:
+                groceryList[pro][0] = qty
+                budget += groceryList[pro][1]
+                groceryList[pro][1] = amt
+            else:
+                groceryList[pro] = [qty, amt]
+        budget -= amt
+    
+    elif budget == 0 or choice == 2:
+        break
+    print("Amount left: ",budget)
 
 canBuy = []    
 for key,val in groceryList.items():
-	if val[1] <= budget:
-		budget -= val[1]
-		canBuy.append(key)
-	if budget == 0:
-		break
+    if val[1] <= budget:
+        budget -= val[1]
+        canBuy.append(key)
+    if budget == 0:
+        break
 
 toBuy = "Amount left can buy you "
 for item in canBuy:
-	toBuy += item + " "
+    toBuy += item + " "
 if toBuy != "Amount left can buy you ":
     print(toBuy)
 
